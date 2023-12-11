55ec3d764750e15f5e8a95880f6b0f7aaf87a0b4
ALLUE <151721459+lallue@users.noreply.github.com>
2023-12-11 14:36:36 +0100

    procedure

--- script.sql
+++ script.sql
@@ -89,66 +89,85 @@ CREATE OR REPLACE PACKAGE BODY PackFasseBouc AS
     PROCEDURE ajouterUtilisateur(p_loginUtilisateur VARCHAR(50), p_nom VARCHAR(50), p_prenom VARCHAR(50), p_anniversaire DATE) IS
     BEGIN
         -- Code pour ajouter un utilisateur
+        INSERT INTO Utilisateur(loginUtilisateur, nom, prenom, anniversaire)
+        VALUES (p_loginUtilisateur, p_nom, p_prenom, p_anniversaire);
     END ajouterUtilisateur;
 
     PROCEDURE supprimerUtilisateur(p_loginUtilisateur VARCHAR(50)) IS
     BEGIN
         -- Code pour supprimer un utilisateur
+        DELETE FROM Utilisateur WHERE loginUtilisateur = p_loginUtilisateur;
     END supprimerUtilisateur;
 
     PROCEDURE connexion(p_loginUtilisateur VARCHAR(50)) IS
     BEGIN
         -- Code pour connecter un utilisateur
+        -- Vous pouvez définir une variable de session pour suivre l'utilisateur connecté, etc.
     END connexion;
 
     PROCEDURE deconnexion IS
     BEGIN
         -- Code pour déconnecter l'utilisateur courant
+        -- Vous pouvez effacer les variables de session, etc.
     END deconnexion;
 
     PROCEDURE ajouterAmi(p_loginUtilisateur VARCHAR(50), p_loginAmi VARCHAR(50)) IS
     BEGIN
         -- Code pour ajouter un ami
+        INSERT INTO Sympathiser(loginUtilisateur1, loginUtilisateur2)
+        VALUES (p_loginUtilisateur, p_loginAmi);
     END ajouterAmi;
 
     PROCEDURE supprimerAmi(p_loginUtilisateur VARCHAR(50), p_loginAmi VARCHAR(50)) IS
     BEGIN
         -- Code pour supprimer un ami
+        DELETE FROM Sympathiser
+        WHERE (loginUtilisateur1 = p_loginUtilisateur AND loginUtilisateur2 = p_loginAmi)
+           OR (loginUtilisateur1 = p_loginAmi AND loginUtilisateur2 = p_loginUtilisateur);
     END supprimerAmi;
 
     PROCEDURE afficherMur(p_loginUtilisateur VARCHAR(50)) IS
     BEGIN
         -- Code pour afficher le mur d'un utilisateur
+        -- Vous pouvez sélectionner les messages du mur dans la table Message
     END afficherMur;
 
-    PROCEDURE ajouterMessageMur(p_loginUtilisateurE VARCHAR(50), p_loginUtilisateurR VARCHAR(50), p_message VARCHAR(200)) IS
+    PROCEDURE ajouterMessageMur(p_loginUtilisateurE VARCHAR(50), p_loginUtilisateurR VARCHAR(50), p_message VARCHAR(50)) IS
     BEGIN
         -- Code pour ajouter un message sur le mur
+        INSERT INTO Message(idMessage, message, datePublication, loginUtilisateurE, loginUtilisateurR)
+        VALUES (1, p_message, CURRENT_DATE, p_loginUtilisateurE, p_loginUtilisateurR);
     END ajouterMessageMur;
 
     PROCEDURE supprimerMessageMur(p_idMessage INT) IS
     BEGIN
         -- Code pour supprimer un message du mur
+        DELETE FROM Message WHERE idMessage = p_idMessage;
     END supprimerMessageMur;
 
-    PROCEDURE repondreMessageMur(p_idMessage INT, p_loginUtilisateur VARCHAR(50), p_messageReponse VARCHAR(200)) IS
+    PROCEDURE repondreMessageMur(p_idMessage INT, p_loginUtilisateur VARCHAR(50), p_messageReponse VARCHAR(50)) IS
     BEGIN
         -- Code pour répondre à un message sur le mur
+        INSERT INTO ReponseMessage(idReponse, message, datePublication, idMessageParent, loginUtilisateur)
+        VALUES (1, p_messageReponse, CURRENT_DATE, p_idMessage, p_loginUtilisateur);
     END repondreMessageMur;
 
     PROCEDURE afficherAmi(p_loginUtilisateur VARCHAR(50)) IS
     BEGIN
         -- Code pour afficher la liste d'amis d'un utilisateur
+        -- Vous pouvez sélectionner les amis dans la table Sympathiser
     END afficherAmi;
 
     PROCEDURE compterAmi(p_loginUtilisateur VARCHAR(50)) IS
     BEGIN
         -- Code pour compter le nombre d'amis d'un utilisateur
+        -- Vous pouvez utiliser une requête COUNT sur la table Sympathiser
     END compterAmi;
 
     PROCEDURE chercherMembre(p_prefixeLoginMembre VARCHAR(50)) IS
     BEGIN
-        -- Code pour chercher un membre du FasseBouc par préfixe de login
+        -- Code pour chercher un membre par préfixe de login
+        -- Vous pouvez utiliser une requête LIKE sur la table Utilisateur
     END chercherMembre;
 
 END PackFasseBouc;
