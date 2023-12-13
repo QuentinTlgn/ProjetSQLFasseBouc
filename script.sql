e8133682d61f92668901508950cd8511f5eaf5df
Quentin <151721294+QuentinTlgn@users.noreply.github.com>
2023-12-13 16:09:23 +0100

    arigato gpt chan

--- script.sql
+++ script.sql
@@ -57,31 +57,32 @@ CREATE TABLE Sympathiser(
 CREATE OR REPLACE PACKAGE PackFasseBouc AS
 
     PROCEDURE ajouterUtilisateur(p_loginUtilisateur IN utilisateur.loginUtilisateur%TYPE, p_nom IN utilisateur.nom%TYPE, p_prenom IN utilisateur.nom%TYPE, p_anniversaire IN utilisateur.anniversaire%TYPE);
-    /*
-    PROCEDURE supprimerUtilisateur(p_loginUtilisateur IN utilisateur.loginUtilisateur%TYPE); 
 
+    PROCEDURE supprimerUtilisateur(p_loginUtilisateur IN utilisateur.loginUtilisateur%TYPE); 
+    /*
     PROCEDURE connexion(p_loginUtilisateur IN utilisateur.loginUtilisateur%TYPE);
 
     PROCEDURE deconnexion;
+    
+    PROCEDURE afficherAmi(p_loginUtilisateur IN utilisateur.loginUtilisateur%TYPE );
+    
+    PROCEDURE afficherMur(p_loginUtilisateur IN utilisateur.loginUtilisateur%TYPE);
+    
+    PROCEDURE compterAmi(p_loginUtilisateur IN utilisateur.loginUtilisateur%TYPE );
+    
+    PROCEDURE chercherMembre(p_prefixeLoginMembre IN VARCHAR);
 
+*/
     PROCEDURE ajouterAmi(p_loginUtilisateur IN utilisateur.loginUtilisateur%TYPE, p_loginAmi IN utilisateur.loginUtilisateur%TYPE);
 
     PROCEDURE supprimerAmi(p_loginUtilisateur IN utilisateur.loginUtilisateur%TYPE , p_loginAmi IN utilisateur.loginUtilisateur%TYPE);
-
-    PROCEDURE afficherMur(p_loginUtilisateur IN utilisateur.loginUtilisateur%TYPE);
-
-    PROCEDURE ajouterMessageMur(p_loginUtilisateurE IN utilisateur.loginUtilisateur%TYPE, p_loginUtilisateurR IN utilisateur.loginUtilisateur%TYPE, p_message IN message.message%TYPE);
+/*
+    PROCEDURE ajouterMessageMur(p_loginUtilisateurE IN utilisateur.loginUtilisateur%TYPE, p_message IN message.message%TYPE);
 
     PROCEDURE supprimerMessageMur(p_idMessage IN message.idMessage%TYPE);
 
     PROCEDURE repondreMessageMur(p_idMessage IN message.idMessage%TYPE, p_loginUtilisateur IN utilisateur.loginUtilisateur%TYPE , p_messageReponse IN message.message%TYPE);
-
-    PROCEDURE afficherAmi(p_loginUtilisateur IN utilisateur.loginUtilisateur%TYPE );
-
-    PROCEDURE compterAmi(p_loginUtilisateur IN utilisateur.loginUtilisateur%TYPE );
-
-    PROCEDURE chercherMembre(p_prefixeLoginMembre IN VARCHAR);
-    */
+*/
 END PackFasseBouc;
 /
 -- Corps des procédures stockées
@@ -93,66 +94,80 @@ CREATE OR REPLACE PACKAGE BODY PackFasseBouc IS
         -- Code pour ajouter un utilisateur
         INSERT INTO utilisateur VALUES(p_loginUtilisateur,p_nom,p_prenom,p_anniversaire);
     END ajouterUtilisateur;
-/*
+
     PROCEDURE supprimerUtilisateur(p_loginUtilisateur IN utilisateur.loginUtilisateur%TYPE) IS
     BEGIN
         -- Code pour supprimer un utilisateur
+        DELETE FROM utilisateur WHERE loginUtilisateur = p_loginUtilisateur;
     END supprimerUtilisateur;
-
+/*
     PROCEDURE connexion(p_loginUtilisateur IN utilisateur.loginUtilisateur%TYPE) IS
     BEGIN
         -- Code pour connecter un utilisateur
+        -- (Ajoutez votre logique de connexion ici)
     END connexion;
 
     PROCEDURE deconnexion IS
     BEGIN
         -- Code pour déconnecter l'utilisateur courant
+        -- (Ajoutez votre logique de déconnexion ici)
     END deconnexion;
-
-    PROCEDURE ajouterAmi(p_loginUtilisateur IN utilisateur.loginUtilisateur%TYPE, p_loginAmi IN utilisateur.loginUtilisateur%TYPE) IS
-    BEGIN
-        -- Code pour ajouter un ami
-    END ajouterAmi;
-
-    PROCEDURE supprimerAmi(p_loginUtilisateur IN utilisateur.loginUtilisateur%TYPE , p_loginAmi ) IS
-    BEGIN
-        -- Code pour supprimer un ami
-    END supprimerAmi;
-
-    PROCEDURE afficherMur(p_loginUtilisateur IN utilisateur.loginUtilisateur%TYPE ) IS
+    
+    PROCEDURE afficherMur(p_loginUtilisateur IN utilisateur.loginUtilisateur%TYPE) IS
     BEGIN
         -- Code pour afficher le mur d'un utilisateur
+        -- (Ajoutez votre logique d'affichage du mur ici)
     END afficherMur;
-
-    PROCEDURE ajouterMessageMur(p_loginUtilisateur IN utilisateur.loginUtilisateur%TYPEE , p_loginUtilisateur IN utilisateur.loginUtilisateur%TYPER , p_message VARCHAR(200)) IS
-    BEGIN
-        -- Code pour ajouter un message sur le mur
-    END ajouterMessageMur;
-
-    PROCEDURE supprimerMessageMur(p_idMessage IN message.idMessage%TYPE) IS
-    BEGIN
-        -- Code pour supprimer un message du mur
-    END supprimerMessageMur;
-
-    PROCEDURE repondreMessageMur(p_idMessage IN message.idMessage%TYPE, p_loginUtilisateur IN utilisateur.loginUtilisateur%TYPE , p_messageReponse IN message.message%TYPE) IS
-    BEGIN
-        -- Code pour répondre à un message sur le mur
-    END repondreMessageMur;
-
+    
     PROCEDURE afficherAmi(p_loginUtilisateur IN utilisateur.loginUtilisateur%TYPE) IS
     BEGIN
         -- Code pour afficher la liste d'amis d'un utilisateur
+        -- (Ajoutez votre logique d'affichage des amis ici)
     END afficherAmi;
 
-    PROCEDURE compterAmi(p_loginUtilisateur IN utilisateur.loginUtilisateur%TYPE ) IS
+    PROCEDURE compterAmi(p_loginUtilisateur IN utilisateur.loginUtilisateur%TYPE) IS
     BEGIN
         -- Code pour compter le nombre d'amis d'un utilisateur
+        -- (Ajoutez votre logique de comptage des amis ici)
     END compterAmi;
-
-    PROCEDURE chercherMembre(p_prefixeLoginMembre IN VARCHAR) IS
+    
+    PROCEDURE chercherMembre(p_prefixeLoginMembre IN VARCHAR2) IS
     BEGIN
-        -- Code pour chercher un membre du FasseBouc par préfixe de login
+        -- Code pour chercher un membre par préfixe de login
+        -- (Ajoutez votre logique de recherche de membre ici)
     END chercherMembre;
+*/
+    PROCEDURE ajouterAmi(p_loginUtilisateur IN utilisateur.loginUtilisateur%TYPE, p_loginAmi IN utilisateur.loginUtilisateur%TYPE) IS
+    BEGIN
+        -- Code pour ajouter un ami
+        INSERT INTO sympathiser VALUES (p_loginUtilisateur, p_loginAmi);
+    END ajouterAmi;
+
+    PROCEDURE supprimerAmi(p_loginUtilisateur IN utilisateur.loginUtilisateur%TYPE, p_loginAmi IN utilisateur.loginUtilisateur%TYPE) IS
+    BEGIN
+        -- Code pour supprimer un ami
+        DELETE FROM sympathiser
+        WHERE (loginUtilisateur1 = p_loginUtilisateur AND loginUtilisateur2 = p_loginAmi)
+           OR (loginUtilisateur1 = p_loginAmi AND loginUtilisateur2 = p_loginUtilisateur);
+    END supprimerAmi;
+/*
+    PROCEDURE ajouterMessageMur(p_loginUtilisateurE IN utilisateur.loginUtilisateur%TYPE, p_loginUtilisateurR IN utilisateur.loginUtilisateur%TYPE, p_message IN message.message%TYPE) IS
+    BEGIN
+        -- Code pour ajouter un message sur le mur
+        INSERT INTO message VALUES (1, p_message, SYSDATE, p_loginUtilisateurE, p_loginUtilisateurR);
+    END ajouterMessageMur;
+
+    PROCEDURE supprimerMessageMur(p_idMessage IN NUMBER) IS
+    BEGIN
+        -- Code pour supprimer un message du mur
+        DELETE FROM message WHERE idMessage = p_idMessage;
+    END supprimerMessageMur;
+
+    PROCEDURE repondreMessageMur(p_idMessage IN NUMBER, p_loginUtilisateur IN utilisateur.loginUtilisateur%TYPE, p_messageReponse IN VARCHAR2) IS
+    BEGIN
+        -- Code pour répondre à un message sur le mur
+        INSERT INTO ReponseMessage VALUES (p_idMessage, p_loginUtilisateur, p_messageReponse, SYSDATE);
+    END repondreMessageMur;
 */
 END PackFasseBouc;
 
@@ -160,9 +175,22 @@ END PackFasseBouc;
 -- Execution des procédures
 -- --------------------------
 
-EXECUTE PackFasseBouc.ajouterUtilisateur('alluel', 'allue', 'luc', '23/04/2000');
 
-/*
-SELECT * FROM USER_OBJECTS WHERE OBJECT_NAME = 'PACKFASSEBOUC' AND OBJECT_TYPE IN ('PACKAGE', 'PACKAGE BODY');
+
+EXECUTE PackFasseBouc.ajouterUtilisateur('alluel', 'allue', 'luc', '23/04/2000');
+EXECUTE PackFasseBouc.ajouterUtilisateur('tauleigq', 'tauleigne', 'quentin', '28/03/2002');
+EXECUTE PackFasseBouc.ajouterUtilisateur('toto', 'to', 'to', '28/03/2002');
+
+SELECT * FROM utilisateur;
+
+EXECUTE PackFasseBouc.supprimerUtilisateur('toto');
+
+EXECUTE PackFasseBouc.ajouterAmi('tauleigq','alluel');
+EXECUTE PackFasseBouc.ajouterAmi('tauleigq','toto');
+SELECT * FROM sympathiser;
+
+EXECUTE PackFasseBouc.supprimerAmi('toto','tauleigq');
+
+/*SELECT * FROM USER_OBJECTS WHERE OBJECT_NAME = 'PACKFASSEBOUC' AND OBJECT_TYPE IN ('PACKAGE', 'PACKAGE BODY');
 
 DROP PACKAGE BODY PACKFASSEBOUC;*/
