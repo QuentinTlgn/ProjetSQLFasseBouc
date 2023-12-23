b835685eb6c2e04189f45813f7c2af4fadd74d4b
ALLUE <151721459+lallue@users.noreply.github.com>
2023-12-23 20:53:27 +0100

    Update script.sql

--- script.sql
+++ script.sql
@@ -1,4 +1,4 @@
---SET SERVEROUTPUT ON;
+SET SERVEROUTPUT ON;
 -- --------------------------
 -- Création des tables
 -- --------------------------
@@ -63,6 +63,7 @@ BEGIN
 END;
 
 CREATE OR REPLACE TRIGGER t_notifCreationMessageMur AFTER INSERT ON Message FOR EACH ROW
+DECLARE
 BEGIN 
   dbms_output.put_line('Nouveau message de '||:NEW.loginUtilisateurE||' sur le mur de '||:NEW.loginUtilisateurR);
 END;
@@ -72,7 +73,6 @@ END;
 -- --------------------------
 -- Création des procédures stockées
 -- --------------------------
-
 
 -- Création des procédures stockées
 CREATE OR REPLACE PACKAGE PackFasseBouc AS
@@ -193,168 +193,167 @@ CREATE OR REPLACE PACKAGE BODY PackFasseBouc AS
     END connexion;
     
     -- Procédure pour afficher l'utilisateur connecté
-PROCEDURE afficherConnecte IS
-BEGIN
-    dbms_output.put_line(utilisateurConnecte);
-END afficherConnecte;
-
--- Procédure pour déconnecter l'utilisateur courant
-PROCEDURE deconnexion IS
-BEGIN
-    -- Code pour déconnecter l'utilisateur courant
-    IF utilisateurConnecte IS NOT NULL THEN
-        utilisateurConnecte := NULL;
-        dbms_output.put_line('Vous nous quittez déjà ? :(');
-    ELSE
-        dbms_output.put_line('Aucun utilisateur connecté');
-    END IF;
-END deconnexion;
-
--- Procédure pour compter le nombre d'amis d'un utilisateur
-PROCEDURE compterAmi IS
-    numAmi INT;
-BEGIN
-    -- Code pour compter le nombre d'amis d'un utilisateur
-    IF utilisateurConnecte IS NOT NULL THEN
-        SELECT COUNT(*) INTO numAmi FROM Sympathiser WHERE loginUtilisateur1 = utilisateurConnecte OR loginUtilisateur2 = utilisateurConnecte;
-        dbms_output.put_line('Vous avez ' || numAmi || ' ami(s)');
-    ELSE
-        dbms_output.put_line('Vous devez être connecté pour effectuer cette action');
-    END IF;
-END compterAmi;
-
--- Procédure pour afficher la liste d'amis d'un utilisateur
-PROCEDURE afficherAmi IS
-BEGIN
-    -- Code pour afficher la liste d'amis d'un utilisateur
-    IF utilisateurConnecte IS NOT NULL THEN
-        FOR ami_rec IN (SELECT DISTINCT CASE WHEN loginUtilisateur1 = utilisateurConnecte THEN loginUtilisateur2 ELSE loginUtilisateur1 END AS ami FROM sympathiser WHERE utilisateurConnecte IN (loginUtilisateur1, loginUtilisateur2)) 
-        LOOP
-            DBMS_OUTPUT.PUT_LINE('Ami : ' || ami_rec.ami);
-        END LOOP;
-    ELSE
-        DBMS_OUTPUT.PUT_LINE('Vous devez être connecté pour afficher la liste d''amis.');
-    END IF;
-END afficherAmi;
-
-    -- PROCEDURE chercherMembre : Cherche un membre par préfixe de login
-PROCEDURE chercherMembre(p_prefixeLoginMembre IN utilisateur.loginUtilisateur%TYPE) IS
-BEGIN
-    -- Vérifie si un utilisateur est connecté
-    IF utilisateurConnecte IS NOT NULL THEN
-        -- Boucle sur les utilisateurs correspondant au préfixe du login
-        FOR v_Utilisateur IN (SELECT DISTINCT loginutilisateur FROM Utilisateur WHERE loginUtilisateur LIKE ('%'||p_prefixeLoginMembre||'%')) 
-        LOOP
-            DBMS_OUTPUT.PUT_LINE('Utilisateur : '||v_Utilisateur.loginutilisateur);
-        END LOOP;
-    ELSE
-        dbms_output.put_line('Vous devez être connecté pour effectuer cette action');
-    END IF;
-END chercherMembre;
-
--- PROCEDURE afficherMur : Affiche le mur d'un utilisateur avec les réponses aux posts
-PROCEDURE afficherMur(p_loginUtilisateur IN utilisateur.loginUtilisateur%TYPE) IS
-BEGIN
-    -- Vérifie si un utilisateur est connecté
-    IF utilisateurConnecte IS NOT NULL THEN
-        DBMS_OUTPUT.PUT_LINE('Mur de '||p_loginUtilisateur);
-        -- Boucle sur les messages et les réponses associées
-        FOR post IN (SELECT DISTINCT m.idMessage, m.message, m.datePublication, m.loginUtilisateurE, r.loginUtilisateurE AS reponse_auteur, r.message AS reponse_message, r.datePublication AS reponse_date
-            FROM Message m
-            LEFT JOIN ReponseMessage r ON m.idMessage = r.idMessageParent
-            WHERE m.loginUtilisateurR = p_loginUtilisateur) 
-        LOOP
-            DBMS_OUTPUT.PUT_LINE('Post '||post.idMessage||' de '||post.loginUtilisateurE|| ' posté le '|| post.datePublication ||' :'|| CHR(10) || post.message|| CHR(10)|| '--------------');
-            -- Affiche la réponse associée, le cas échéant
-            IF post.reponse_message IS NOT NULL THEN
-                DBMS_OUTPUT.PUT_LINE('Réponse de ' || post.reponse_auteur || ' le ' || post.reponse_date || ': ' || post.reponse_message|| CHR(10) || '--------------');
+    PROCEDURE afficherConnecte IS
+    BEGIN
+        dbms_output.put_line(utilisateurConnecte);
+    END afficherConnecte;
+    
+    -- Procédure pour déconnecter l'utilisateur courant
+    PROCEDURE deconnexion IS
+    BEGIN
+        -- Code pour déconnecter l'utilisateur courant
+        IF utilisateurConnecte IS NOT NULL THEN
+            utilisateurConnecte := NULL;
+            dbms_output.put_line('Vous nous quittez déjà ? :(');
+        ELSE
+            dbms_output.put_line('Aucun utilisateur connecté');
+        END IF;
+    END deconnexion;
+    
+    -- Procédure pour compter le nombre d'amis d'un utilisateur
+    PROCEDURE compterAmi IS
+        numAmi INT;
+    BEGIN
+        -- Code pour compter le nombre d'amis d'un utilisateur
+        IF utilisateurConnecte IS NOT NULL THEN
+            SELECT COUNT(*) INTO numAmi FROM Sympathiser WHERE loginUtilisateur1 = utilisateurConnecte OR loginUtilisateur2 = utilisateurConnecte;
+            dbms_output.put_line('Vous avez ' || numAmi || ' ami(s)');
+        ELSE
+            dbms_output.put_line('Vous devez être connecté pour effectuer cette action');
+        END IF;
+    END compterAmi;
+    
+    -- Procédure pour afficher la liste d'amis d'un utilisateur
+    PROCEDURE afficherAmi IS
+    BEGIN
+        -- Code pour afficher la liste d'amis d'un utilisateur
+        IF utilisateurConnecte IS NOT NULL THEN
+            FOR ami_rec IN (SELECT DISTINCT CASE WHEN loginUtilisateur1 = utilisateurConnecte THEN loginUtilisateur2 ELSE loginUtilisateur1 END AS ami FROM sympathiser WHERE utilisateurConnecte IN (loginUtilisateur1, loginUtilisateur2)) 
+            LOOP
+                DBMS_OUTPUT.PUT_LINE('Ami : ' || ami_rec.ami);
+            END LOOP;
+        ELSE
+            DBMS_OUTPUT.PUT_LINE('Vous devez être connecté pour afficher la liste d''amis.');
+        END IF;
+    END afficherAmi;
+    
+        -- PROCEDURE chercherMembre : Cherche un membre par préfixe de login
+    PROCEDURE chercherMembre(p_prefixeLoginMembre IN utilisateur.loginUtilisateur%TYPE) IS
+    BEGIN
+        -- Vérifie si un utilisateur est connecté
+        IF utilisateurConnecte IS NOT NULL THEN
+            -- Boucle sur les utilisateurs correspondant au préfixe du login
+            FOR v_Utilisateur IN (SELECT DISTINCT loginutilisateur FROM Utilisateur WHERE loginUtilisateur LIKE ('%'||p_prefixeLoginMembre||'%')) 
+            LOOP
+                DBMS_OUTPUT.PUT_LINE('Utilisateur : '||v_Utilisateur.loginutilisateur);
+            END LOOP;
+        ELSE
+            dbms_output.put_line('Vous devez être connecté pour effectuer cette action');
+        END IF;
+    END chercherMembre;
+    
+    -- PROCEDURE afficherMur : Affiche le mur d'un utilisateur avec les réponses aux posts
+    PROCEDURE afficherMur(p_loginUtilisateur IN utilisateur.loginUtilisateur%TYPE) IS
+    BEGIN
+        -- Vérifie si un utilisateur est connecté
+        IF utilisateurConnecte IS NOT NULL THEN
+            DBMS_OUTPUT.PUT_LINE('Mur de '||p_loginUtilisateur);
+            -- Boucle sur les messages et les réponses associées
+            FOR post IN (SELECT DISTINCT m.idMessage, m.message, m.datePublication, m.loginUtilisateurE, r.loginUtilisateurE AS reponse_auteur, r.message AS reponse_message, r.datePublication AS reponse_date
+                FROM Message m
+                LEFT JOIN ReponseMessage r ON m.idMessage = r.idMessageParent
+                WHERE m.loginUtilisateurR = p_loginUtilisateur ORDER BY m.datePublication DESC, r.datePublication)
+            LOOP
+                DBMS_OUTPUT.PUT_LINE(CHR(10) || 'Post '||post.idMessage||' de '||post.loginUtilisateurE|| ' posté le '|| post.datePublication ||' :'|| CHR(10) || post.message|| CHR(10)|| '--------------');
+                -- Affiche la réponse associée, le cas échéant
+                IF post.reponse_message IS NOT NULL THEN
+                    DBMS_OUTPUT.PUT_LINE('Réponse de ' || post.reponse_auteur || ' le ' || post.reponse_date || ': ' || post.reponse_message|| CHR(10) || '--------------');
+                END IF;
+            END LOOP;
+        ELSE
+            dbms_output.put_line('Vous devez être connecté pour effectuer cette action');
+        END IF;
+    END afficherMur;
+    
+        -- PROCEDURE ajouterMessageMur : Ajoute un message au mur d'un utilisateur
+    PROCEDURE ajouterMessageMur(p_loginUtilisateurR IN utilisateur.loginUtilisateur%TYPE, p_message IN message.message%TYPE) IS
+        v_amitie_existe NUMBER := 0;
+    BEGIN
+        -- Vérifie si un utilisateur est connecté
+        IF utilisateurConnecte IS NOT NULL THEN
+            -- Vérifier si l'amitié existe déjà
+            SELECT COUNT(*) INTO v_amitie_existe
+            FROM Sympathiser
+            WHERE (loginUtilisateur1, loginUtilisateur2) IN ((utilisateurConnecte, p_loginUtilisateurR), (p_loginUtilisateurR, utilisateurConnecte));
+            
+            -- Si l'amitié existe ou si l'utilisateur est lui-même, ajouter le message au mur
+            IF v_amitie_existe > 0 OR utilisateurConnecte =  p_loginUtilisateurR THEN
+                INSERT INTO message VALUES (1, p_message, SYSDATE, utilisateurConnecte, p_loginUtilisateurR);
+                COMMIT;
+            ELSE
+                dbms_output.put_line('Vous devez être ami avec cette personne pour pouvoir effectuer cette action');
             END IF;
-        END LOOP;
-    ELSE
-        dbms_output.put_line('Vous devez être connecté pour effectuer cette action');
-    END IF;
-END afficherMur;
-
-    -- PROCEDURE ajouterMessageMur : Ajoute un message au mur d'un utilisateur
-PROCEDURE ajouterMessageMur(p_loginUtilisateurR IN utilisateur.loginUtilisateur%TYPE, p_message IN message.message%TYPE) IS
-    v_amitie_existe NUMBER := 0;
-BEGIN
-    -- Vérifie si un utilisateur est connecté
-    IF utilisateurConnecte IS NOT NULL THEN
-        -- Vérifier si l'amitié existe déjà
-        SELECT COUNT(*) INTO v_amitie_existe
-        FROM Sympathiser
-        WHERE (loginUtilisateur1, loginUtilisateur2) IN ((utilisateurConnecte, p_loginUtilisateurR), (p_loginUtilisateurR, utilisateurConnecte));
-        
-        -- Si l'amitié existe ou si l'utilisateur est lui-même, ajouter le message au mur
-        IF v_amitie_existe > 0 OR utilisateurConnecte =  p_loginUtilisateurR THEN
-            INSERT INTO message VALUES (1, p_message, SYSDATE, utilisateurConnecte, p_loginUtilisateurR);
-            COMMIT;
         ELSE
-            dbms_output.put_line('Vous devez être ami avec cette personne pour pouvoir effectuer cette action');
+            DBMS_OUTPUT.PUT_LINE('Vous devez être connecté pour effectuer cette action');
         END IF;
-    ELSE
-        DBMS_OUTPUT.PUT_LINE('Vous devez être connecté pour effectuer cette action');
-    END IF;
-END ajouterMessageMur;
-
--- PROCEDURE supprimerMessageMur : Supprime un message du mur d'un utilisateur
-PROCEDURE supprimerMessageMur(p_idMessage IN message.idMessage%TYPE) IS
-    idReceveur utilisateur.loginUtilisateur%TYPE;
-BEGIN
-    -- Vérifie si un utilisateur est connecté
-    IF utilisateurConnecte IS NOT NULL THEN
-        -- Récupère le login de l'utilisateur receveur du message
-        SELECT loginUtilisateurR INTO idReceveur FROM Message WHERE idMessage = p_idMessage;
-        
-        -- Si l'utilisateur connecté est le receveur, supprimer le message
-        IF idReceveur = utilisateurConnecte THEN
-            DELETE FROM Message WHERE idMessage = p_idMessage;
-            COMMIT;
-            DBMS_OUTPUT.PUT_LINE('Le message a bien été supprimé');
+    END ajouterMessageMur;
+    
+    -- PROCEDURE supprimerMessageMur : Supprime un message du mur d'un utilisateur
+    PROCEDURE supprimerMessageMur(p_idMessage IN message.idMessage%TYPE) IS
+        idReceveur utilisateur.loginUtilisateur%TYPE;
+    BEGIN
+        -- Vérifie si un utilisateur est connecté
+        IF utilisateurConnecte IS NOT NULL THEN
+            -- Récupère le login de l'utilisateur receveur du message
+            SELECT loginUtilisateurR INTO idReceveur FROM Message WHERE idMessage = p_idMessage;
+            
+            -- Si l'utilisateur connecté est le receveur, supprimer le message ainsi que les réponses
+            IF idReceveur = utilisateurConnecte THEN
+                DELETE FROM REPONSEMESSAGE WHERE IDMESSAGEPARENT = p_idMessage;
+                DELETE FROM Message WHERE idMessage = p_idMessage;
+                COMMIT;
+                DBMS_OUTPUT.PUT_LINE('Le message a bien été supprimé');
+            ELSE
+                DBMS_OUTPUT.PUT_LINE('Vous pouvez uniquement supprimer les messages de votre mur');
+            END IF;
         ELSE
-            DBMS_OUTPUT.PUT_LINE('Vous pouvez uniquement supprimer les messages de votre mur');
+            dbms_output.put_line('Vous devez être connecté pour effectuer cette action');
         END IF;
-    ELSE
-        dbms_output.put_line('Vous devez être connecté pour effectuer cette action');
-    END IF;
-END supprimerMessageMur;
-
--- PROCEDURE repondreMessageMur : Répond à un message sur le mur d'un utilisateur
-PROCEDURE repondreMessageMur(p_idMessage IN message.idMessage%TYPE, p_messageReponse IN ReponseMessage.message%TYPE) IS
-    idMessage message.idMessage%TYPE;
-    idReceveur message.loginUtilisateurR%TYPE;
-    v_amitie_existe NUMBER := 0;
-BEGIN
-    -- Vérifie si un utilisateur est connecté
-    IF utilisateurConnecte IS NOT NULL THEN
-        -- Récupère l'ID du message et le login du receveur
-        SELECT idMessage, loginUtilisateurR INTO idMessage, idReceveur FROM Message WHERE idMessage = p_idMessage;
-        
-        -- Vérifie si l'amitié existe
-        SELECT COUNT(*) INTO v_amitie_existe
-        FROM Sympathiser
-        WHERE (loginUtilisateur1, loginUtilisateur2) IN ((utilisateurConnecte, idReceveur), (idReceveur, utilisateurConnecte));
-        
-        -- Si l'amitié existe et l'ID du message est valide, ajouter la réponse au message
-        IF (v_amitie_existe > 0 OR utilisateurConnecte =  idReceveur) AND idMessage IS NOT NULL THEN
-            INSERT INTO ReponseMessage VALUES (p_idMessage, utilisateurConnecte, p_messageReponse, SYSDATE);
-            COMMIT;
+    END supprimerMessageMur;
+    
+    -- PROCEDURE repondreMessageMur : Répond à un message sur le mur d'un utilisateur
+    PROCEDURE repondreMessageMur(p_idMessage IN message.idMessage%TYPE, p_messageReponse IN ReponseMessage.message%TYPE) IS
+        idMessage message.idMessage%TYPE;
+        idReceveur message.loginUtilisateurR%TYPE;
+        v_amitie_existe NUMBER := 0;
+    BEGIN
+        -- Vérifie si un utilisateur est connecté
+        IF utilisateurConnecte IS NOT NULL THEN
+            -- Récupère l'ID du message et le login du receveur
+            SELECT idMessage, loginUtilisateurR INTO idMessage, idReceveur FROM Message WHERE idMessage = p_idMessage;
+            
+            -- Vérifie si l'amitié existe
+            SELECT COUNT(*) INTO v_amitie_existe
+            FROM Sympathiser
+            WHERE (loginUtilisateur1, loginUtilisateur2) IN ((utilisateurConnecte, idReceveur), (idReceveur, utilisateurConnecte));
+            
+            -- Si l'amitié existe et l'ID du message est valide, ajouter la réponse au message
+            IF (v_amitie_existe > 0 OR utilisateurConnecte =  idReceveur) AND idMessage IS NOT NULL THEN
+                INSERT INTO ReponseMessage VALUES (p_idMessage, utilisateurConnecte, p_messageReponse, SYSDATE);
+                COMMIT;
+            ELSE
+                dbms_output.put_line('Vous devez être ami avec cette personne pour pouvoir effectuer cette action');
+            END IF;
         ELSE
-            dbms_output.put_line('Vous devez être ami avec cette personne pour pouvoir effectuer cette action');
+            dbms_output.put_line('Vous devez être connecté pour effectuer cette action');
         END IF;
-    ELSE
-        dbms_output.put_line('Vous devez être connecté pour effectuer cette action');
-    END IF;
-END repondreMessageMur;
-
-END PackFasseBouc;
-
+    END repondreMessageMur;
+    
+    END PackFasseBouc;
 
 -- --------------------------
 -- Execution des procédures
 -- --------------------------
-
 
 EXECUTE PackFasseBouc.ajouterUtilisateur('alluel', 'allue', 'luc', '23/04/2000');
 EXECUTE PackFasseBouc.ajouterUtilisateur('tauleigq', 'tauleigne', 'quentin', '28/03/2002');
@@ -374,19 +373,16 @@ EXECUTE PackFasseBouc.afficherAmi;
 EXECUTE PackFasseBouc.afficherConnecte;
 EXECUTE PackFasseBouc.deconnexion;
 
+
 EXECUTE PackFasseBouc.compterAmi;
 
 EXECUTE PackFasseBouc.chercherMembre('all');
 
 EXECUTE PackFasseBouc.afficherMur('alluel');
 
-EXECUTE PackFasseBouc.supprimerMessageMur(1003);
+EXECUTE PackFasseBouc.supprimerMessageMur(2);
 
-EXECUTE PackFasseBouc.ajouterMessageMur('alluel','onix');
+EXECUTE PackFasseBouc.ajouterMessageMur('tauleigq','onix');
 
-EXECUTE PackFasseBouc.repondreMessageMur(3, 'ouais');
+EXECUTE PackFasseBouc.repondreMessageMur(4, 'bienvue');
 SELECT * FROM ReponseMessage;
-
-SELECT * FROM USER_OBJECTS WHERE OBJECT_NAME = 'PACKFASSEBOUC' AND OBJECT_TYPE IN ('PACKAGE', 'PACKAGE BODY');
-
-DROP PACKAGE BODY PACKFASSEBOUC;
